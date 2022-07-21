b63ae5ee8aa713b56ad36effe5e4de58991de3ca
Jack <71041042+Haxor1611@users.noreply.github.com>
2022-07-21 22:27:56 +0100

    made most things work

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import time
 from PIL import Image
 import pytesseract
 
-currentNumber = 0
+currentNumber = 1
 numberOfGovs = 0
 # Constants for image location
 PROFILE = (83, 92)
 RANKINGS = (660, 807)
 POWERRANKS = (463, 627)
 EXITRANK = (1600, 152)
+MOREINFO = (458, 817)
 
 # first 5 ranks need to be hard coded in as they dont move positions.
 RANK1 = (948, 360)
@@ -20,27 +21,28 @@ RANK4 = (948, 720)
 # the game auto adjusts so no changes are need to be made.
 RANK = (948, 720)
 # locations where the photos are grabbed
-name = (2653, 338, 351, 65)
-ID = (2829, 308, 108, 36)
-KP = (3242, 455, 245, 42)
+name = (757, 335, 331, 64)
+ID = (904, 302, 115,51)
+KP = (1316, 458, 229, 45)
 
 # Paths for all of the files
-DeadPath = r"C:\Users\jackk\__HPQ school project\Dead"
-IdsPath = r"C:\Users\jackk\__HPQ school project\Ids"
-KillsPath = r"C:\Users\jackk\__HPQ school project\Kill Points"
-NamesPath = r"C:\Users\jackk\__HPQ school project\Names"
-PowerPath = r"C:\Users\jackk\__HPQ school project\Power"
-T4Path = r"C:\Users\jackk\__HPQ school project\T4"
-T5Path = r"C:\Users\jackk\__HPQ school project\T5"
+DeadPath = r"C:\Users\jackk\__HPQ school project\HPQ-Project\Dead"
+IdsPath = r"C:\Users\jackk\__HPQ school project\HPQ-Project\Ids"
+KillsPath = r"C:\Users\jackk\__HPQ school project\HPQ-Project\Kill Points"
+NamesPath = r"C:\Users\jackk\__HPQ school project\HPQ-Project\Names"
+PowerPath = r"C:\Users\jackk\__HPQ school project\HPQ-Project\Power"
+T4Path = r"C:\Users\jackk\__HPQ school project\HPQ-Project\T4"
+T5Path = r"C:\Users\jackk\__HPQ school project\HPQ-Project\T5"
 
 def scanImage(filename):
     return pytesseract.image_to_string(Image.open(filename))
 
-
+ 
 
 def grabInfo(location, pathOfType):
-    path = pathOfType + ": " + str(currentNumber) + ".png"
-    image = pyautogui.screenshot(path, region=location)
+    path = (pathOfType + "\ " + str(currentNumber) + ".png")
+    image = pyautogui.screenshot(region=location)
+    image.save(path)
     time.sleep(2)
 
     return scanImage(path)
@@ -55,8 +57,14 @@ class Governor:
         self.t4 = t4
         self.t5 = t5
 
+def click(pos):
+    pyautogui.moveTo(pos)
+    time.sleep(1)
+    pyautogui.click()
 
 def getPlayerInfo():
+    global currentNumber
+
     print("Getting name from governor " + str(currentNumber) + ": ")
     nameOfGov = grabInfo(name, NamesPath)
     print("Governor name: " + nameOfGov)
@@ -66,15 +74,14 @@ def getPlayerInfo():
     print("Getting kill points from governor " + str(currentNumber) + ": ")
     killsOfGov = grabInfo(KP, KillsPath)
     print("Governor kill points: " + killsOfGov)
+
+    #click(MOREINFO)
+    #print("Getting deads from governor + " str(currentNumber) + ": ")
+    currentNumber += 1
     click(EXITRANK)
 
-def click(pos):
-    pyautogui.moveTo(pos)
-    time.sleep(1)
-    pyautogui.click()
-
-
 def mainProc():
+    global numberOfGovs
     click(RANK1)
     time.sleep(1)
     getPlayerInfo()
@@ -100,7 +107,8 @@ def mainProc():
 
 
 def startup():
-    numberOfGovs = input("How many governors inforation should i grab?")
+    global numberOfGovs
+    numberOfGovs = int(input("How many governors inforation should i grab?"))
     click(PROFILE)
     click(RANKINGS)
     click(POWERRANKS)
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import pytesseract
+import pyautogui
 from PIL import Image
-
-print(int((pytesseract.image_to_string(Image.open(r"C:\Users\jackk\__HPQ school project\Dead\test.png"))).replace(",", "")))
+location = pyautogui.locateOnScreen("kp.png")
+print(location)
